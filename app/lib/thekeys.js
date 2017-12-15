a1c4316392ec2d2b4e8d0f251d5426d6de817500
quentin <qb@copark.co>
2017-12-15 10:08:24 +0100

    adding lib thekeys.js

---
+++ app/lib/thekeys.js
@@ -0,0 +1,117 @@
+/**
+ * thekey.js
+ * @author Quentin BERNET
+ * @version 1.0
+ */
+
+/**
+ * @var tkapi
+ * @desc module the keys Android/iOS
+ * @todo : tests fonctionnels
+ */
+var tkapi = require('fr.thekeys.api');
+
+/**
+ * @var userManager
+ * @desc instance of userManager
+ */
+var userManager = null ;
+/**
+ * @var keyManager
+ * @desc instance of keyManager
+ */
+var keyManager = null ;
+/**
+ * @var _username
+ * @desc nom d'utilisateur client the keys
+ */
+var _username = "+33643337991" ;
+/**
+ * @var _password
+ * @desc mot de passe de l'utilisateur the keys
+ */
+var _password = "pe3kon7c" ;
+/**
+ * @var _fences
+ * @desc liste de barrières du parking
+ */
+var _fences = null ;
+
+var doAction = (function() {
+
+  /**
+   * @function open
+   * @param fences map _fences
+   * @desc lance une commande d'ouverture sur le module the keys à proximité
+   */
+  var open = function(fences)
+  {
+    Ti.API.info("Open action has been called from thekeys lib") ;
+    userManager = tkapi.createUserManager();
+    keyManager = tkapi.createKeyManager();
+    Ti.API.info("userManager and keyManager has been well created") ;
+    _fences = fences ;
+    userManager.login(_username,_password, function(result) {
+    	Ti.API.info("Login result is : " + result["status"]);
+    });
+    Ti.API.info("calling function callback");
+    userManager.synchronizeUser(onUserSynchronized);
+  } ;
+  /**
+   * @function onUserSynchronized
+   * @desc récupère les clés autorisées de l'utilisateur
+   * lance le scan et map entre toutes les valeurs
+   * lance finalement une commande d'ouverture si une combinaison valide est trouvée
+   * @todo ajout d'une promesse pour renforcer l'async
+   */
+  function onUserSynchronized() {
+    Ti.API.info("User has been well synchronized. Fetching keys");
+  	allkeys = keyManager.getKeys();
+  	var keys = [] ;
+  	for(k in allkeys) {
+  		var key = {
+  				 identifier: allkeys[k]["identifier"],
+  				 title: allkeys[k]["name"]
+  			};
+  		keys.push(key);
+  	}
+    Ti.API.info("list of granted keys found : ") ;
+    Ti.API.info(JSON.stringify(keys));
+    /**
+     * @function openLocker
+     * lance une commande d'ouverture sur la clé e
+     * @param e clé à ouvrir
+     */
+  	function openLocker(e) {
+      Ti.API.info("Opening locker " + JSON.stringify(e));
+  		keyManager.open(e["identifier"], function(event) {
+  			Ti.API.info("event " + event["status"]) ;
+  		});
+  	}
+    Ti.API.info("start scaning bluetooth devices...");
+    // start scan et match les valeurs avec le BO
+  	keyManager.startScan(function(result) {
+        Ti.API.info("onKeyFound: " + JSON.stringify(result));
+        for(var j = 0 ; j < keys.length ; j++) {
+          var key = keys[j];
+          if(key["identifier"] == result["identifier"]) {
+            for(var k = 0 ; k < _fences.length ; k++) {
+              var fence = _fences[k] ;
+              if((fence.fen_type == "door in" || fence.fen_type == "door out" || fence.fen_type == "door in/out") && fence.fen_code == key["title"].split("Porte ")[1] && fence.fen_valid == 1) {
+                Ti.API.warn("Granted key has been found, calling openLocker " + key["title"]) ;
+                openLocker(result);
+              }
+            }
+          }
+        }
+    });
+
+  }
+
+	return {
+		open:open
+	};
+
+} ());
+
+exports.do = doAction ;
